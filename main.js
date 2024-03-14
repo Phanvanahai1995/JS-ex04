d8164d79623fffa3b52fc8441565e3b9c6a43992
phanvanhai <phanvanhaixdddk49@gmail.com>
2024-03-14 20:53:57 +0700

    up

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let content =
+  "Một con vịt xòe ra hai cái cánh, nó kêu rằng quác quác quác quạc quạc quạc";
+
+content = content.replaceAll(" ", "</span> <span>");
+content = `<span>${content}</span>`;
+console.log(content);
+let i = 0;
+
+setInterval(function () {
+  let firstChar = content.charAt(i);
+  let nextChar = content.charAt(i + 1);
+  let html = "";
+  if (firstChar === ">" && nextChar !== " ") {
+    html = content.slice(0, i) + ` class="red"` + content.slice(i);
+    document.body.innerHTML = html;
+  }
+  if (i === content.length) {
+    i = 0;
+  } else {
+    i++;
+  }
+  i;
+}, 50);
